b8ac77a3331aa3f4f26c4ef7c1d34e4623ab37dd
svideo <83764681+Svideo-pp@users.noreply.github.com>
2021-10-26 03:12:56 +0100

    Update server.py

--- Roberta/server.py
+++ Roberta/server.py
@@ -1,16 +1,12 @@
 # This file uses to deploy on a server
-
 from flask import Flask, request
 from sentence_transformers import SentenceTransformer, util
 
 
-# load sentence similarity models
-model = SentenceTransformer('C:/Users/svideo/Desktop/HuggingFace Model/all-roberta-large-v1')
-print('model all_roberta_large_v1 loaded')
-
-
-#Compute cosine-similarits
+# Compute cosine-similarities
 def sim(sent1, sent2):
+    # load sentence similarity models
+    model = SentenceTransformer('./all-roberta-large-v1')
     embedding1 = model.encode(sent1, convert_to_tensor=True)
     embedding2 = model.encode(sent2, convert_to_tensor=True)
     cosine_score = util.pytorch_cos_sim(embedding1, embedding2)
@@ -21,9 +17,10 @@ app = Flask(__name__)
 # app.config['ENV'] = 'development'
 # app.config['DEBUG'] = True
 
-@app.route('/auto_scoring', methods=['GET', 'POST'])
-def scoring():
-    if (request.method == 'GET'):
+
+@app.route('/auto_grading', methods=['GET', 'POST'])
+def grading():
+    if request.method == 'GET':
         student_answer = request.args['sa']
         reference_answer = request.args['ra']
     else:
@@ -31,6 +28,8 @@ def scoring():
         reference_answer = request.form['ra']
 
     cosine_similarity = sim(student_answer, reference_answer)
+    if cosine_similarity < 0:
+        cosine_similarity = 0.0000
     return {
         "student_answer": student_answer,
         "reference_answer": reference_answer,
@@ -39,8 +38,14 @@ def scoring():
     }
 
 
-try:
-    app.run(port=8080)
-except:
-    print("running error")
+@app.route('/')
+def home():
+    print('[INFO from server.py] Access to home page successful.')
+    return 'Hello, this is home page. I\'m running on docker now.'
 
+
+if __name__ == '__main__':
+    try:
+        app.run(port=5000, debug=True)
+    except RuntimeError as e:
+        print('Mostly like an unexpected input has been sent to the server.')
